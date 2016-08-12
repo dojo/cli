a94e81f7476e1c50b099f39fab14c71dde8e0563
Tom Dye <tom@ursaminor.co.uk>
2016-08-12 12:00:27 +0200

    removing harmony from grunt

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,12 +1,5 @@
 module.exports = function (grunt) {
 	require('grunt-dojo2').initConfig(grunt, {
-		intern: {
-			options: {
-				nodeOptions: [
-					'--harmony',
-					'--harmony_default_parameters'
-				]
-			}
-		}
+		/* any custom configuration goes here */
 	});
 };
