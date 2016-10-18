c406495338e49c894e9112fa5c28d4ff3b7d0daa
Rory Mulligan <rorticus@gmail.com>
2016-10-18 07:42:56 -0500

    Adding jsdoc to explain interfaces/methods

--- src/version.ts
+++ src/version.ts
@@ -11,17 +11,29 @@ import { join } from 'path';
 const pkgDir = require('pkg-dir');
 const packagePath = pkgDir.sync(dirname);
 
+/**
+ * The details of one command group's module.
+ */
 export interface ModuleVersion {
 	name: string;
 	version: string;
 	group: string;
 }
 
+/**
+ * Important information to be retrieved from a module's package.json
+ */
 export interface PackageDetails {
 	name: string;
 	version: string;
 }
 
+/**
+ * Read information about a package/module, if available, or return default values.
+ *
+ * @param {string} packageDir The directory containing the package.json file.
+ * @returns {{name: (any|string), version: any}}
+ */
 export function readPackageDetails(packageDir: string): PackageDetails {
 	let data: any = {};
 	try {
@@ -36,7 +48,14 @@ export function readPackageDetails(packageDir: string): PackageDetails {
 	};
 }
 
-export function buildVersions(commandsMap: CommandsMap) {
+/**
+ * Iterate through a CommandsMap and retrieve the module details of each module referenced in the
+ * CommandsMap. The returned list is sorted in alphabetical order, by group.
+ *
+ * @param {CommandsMap} commandsMap
+ * @returns {{name, version, group}[]}
+ */
+export function buildVersions(commandsMap: CommandsMap): ModuleVersion[] {
 	/*
 	 * commandsMap comes in as a map of [command-name, command]. The command has a default command,
 	 * the map will actually contain two entries for one command, on for the default command, one for the real,
@@ -59,6 +78,13 @@ export function buildVersions(commandsMap: CommandsMap) {
 		}).sort((a, b) => a.group > b.group ? 1 : -1);
 }
 
+/**
+ * Returns a string describing the command group, module name, and module version of each
+ * command referenced in a specified CommandsMap. This is used to print the the help string.
+ *
+ * @param {CommandsMap} commandsMap
+ * @returns {string}
+ */
 export default function createVersionsString(commandsMap: CommandsMap): string {
 	const myPackageDetails = readPackageDetails(packagePath),
 		commands = buildVersions(commandsMap);
