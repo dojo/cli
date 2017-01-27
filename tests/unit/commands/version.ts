ac5313c895d65fafc79801a54736832a92a9e6f6
James Donaghue <james.donaghue@gmail.com>
2017-01-26 20:19:05 -0500

    Fix tests

--- tests/unit/commands/version.ts
+++ tests/unit/commands/version.ts
@@ -39,7 +39,7 @@ describe('version command', () => {
 
 	it('should register supported arguments', () => {
 		const options = sandbox.stub();
-		moduleUnderTest.register(null, options);
+		moduleUnderTest.register(options);
 		assert.deepEqual(
 			options.firstCall.args,
 			[ 'o', {
