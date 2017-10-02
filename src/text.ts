8d9e7a40a213a8f47fbb23e59ee24621a1678ed6
James Milner <jamesmilner22@hotmail.com>
2017-10-02 22:49:44 +0100

    Clarify help command text (#165)

--- src/text.ts
+++ src/text.ts
@@ -8,4 +8,6 @@ Hey there, here are all the things you can do with @dojo/cli:`;
 
 export const helpEpilog = `For more information on any of these commands just run them with '-h'.
 
-e.g. 'dojo build -h' will give you the help for the 'build' group of commands.`;
+e.g. 'dojo build -h' will give you the help for the 'build' group of commands.
+
+If a non-builtin command (e.g. build) appears missing from the command list, please ensure it is listed in package.json and correctly installed.`;
