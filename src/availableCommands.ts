9972a9dafdd76c9a41d22e444ece5da2830545f8
Tom Dye <github@tomdye.co.uk>
2018-01-05 15:00:41 +0000

    loading list

---
+++ src/availableCommands.ts
@@ -0,0 +1,40 @@
+import * as execa from 'execa';
+import { NpmPackageDetails } from './interfaces';
+import * as Configstore from 'configstore';
+
+const INITIAL_TIMEOUT = 3000;
+
+export default async function(name: string) {
+	const conf = new Configstore(name);
+
+	let commands = conf.get('commands') || [];
+	if (!commands.length) {
+		commands = await getLatestCommands(name, conf);
+	}
+
+	return commands;
+}
+
+async function getLatestCommands(packageName: string, conf: Configstore) {
+	let commands;
+
+	try {
+		commands = await search(INITIAL_TIMEOUT);
+		conf.set('commands', commands);
+		conf.set('lastUpdated', Date.now());
+	}
+	catch (error) {
+		// console.log('\nProcess timed out when fetching command list');
+	}
+
+	return commands;
+}
+
+async function search(timeout: number = 0): Promise<NpmPackageDetails[]> {
+	return execa('npm', ['search', '@dojo', 'cli-', '--json'], { timeout }).then(result => {
+		const commands = JSON.parse(result.stdout);
+		return commands.filter(({ name }: NpmPackageDetails) => {
+			return name !== '@dojo/cli';
+		});
+	});
+}
